16d805a6a21b75eec77e76981962e800e67b23f3
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2023-03-31 00:28:49 +0530

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Vithushan Logan on 2023-03-31.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
