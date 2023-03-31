1e0bc89a29b213500c2cedc143dc661c7dfbe792
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2023-03-31 23:07:10 +0530

    ✅ Home Page header added to the file 👨🏻‍💻

--- CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -9,31 +9,8 @@ struct HomeView: View {
         ZStack{
             Color.theme.background
                 .ignoresSafeArea()
-            
-            
             VStack{
-                HStack{
-                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
-                        .animation(.none)
-                        .background(
-                            CircleButtonAnimationView(animate: $showPortfolio)
-                        )
-                    Spacer()
-                    Text(showPortfolio ? "Portfolio" : "Live Prices")
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.theme.accent)
-                        .animation(.none)
-                    Spacer()
-                    CircleButtonView(iconName: "chevron.right")
-                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
-                        .onTapGesture {
-                            withAnimation(.spring()){
-                                showPortfolio.toggle()
-                            }
-                        }
-                }
-              
+                homeHeader
                 Spacer(minLength: 0)
                 
             }
@@ -48,5 +25,33 @@ struct HomeView_Previews: PreviewProvider {
                 .navigationBarHidden(true)
         }
         
+    }
+}
+
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack{
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()){
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
     }
 }
